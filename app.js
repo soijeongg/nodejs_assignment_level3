f3ddc03c9cc81f30d443c29e86978669f2e58943
soijeongg <010127js@gmail.com>
2024-02-27 14:10:37 +0900

    FEAT:make categories

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-//import CategoryRouter from "./routes/category.router.js";
+import CategoryRouter from "./src/routes/category.router.js";
 //import MenuRouter from "./routes/reviews.router.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
-
+app.use("/api/categories", CategoryRouter)
 app.get("/", (req, res) => {
   res.send("<h1>3차과제</h1>");
 });
