600ccf1fe16b1615c6100a95721780cf62bc7101
soijeongg <010127js@gmail.com>
2024-02-28 14:21:53 +0900

    REFACTOR code

--- src/routes/category.router.js
+++ src/routes/category.router.js
@@ -33,11 +33,11 @@ return res.status(200).json({data:category})
 router.post('/', async(req, res,next)=> {
 
     try{//없으면 
-
-    if(!req.body){
+    let { name } = req.body;
+    if(!name){
         return res.status(400),json({errorMessage:"데이터 형식이 올바르지 않습니다"})
     }
-    let {name} = req.body
+    
     const validationResult = schemas.validate({ name });
     if (validationResult.error) {
       return res
@@ -64,7 +64,7 @@ router.put('/:categoryId', async (req, res, next) => {
 try{
   let { categoryId } = req.params;
   const { name, order } = req.body;
-  if (!req.params || !req.body) {
+  if (!categoryId || !name ||!order) {
     return res
       .status(400)
       .json({ message: '데이터 형식이 올바르지 않습니다.' });
@@ -96,7 +96,7 @@ if (validationResult.error){
 router.delete('/:categoryId', async (req, res,next) => {
     try{
   let { categoryId } = req.params;
-  if (!req.params) {
+  if (!categoryId) {
     return res
       .status(400)
       .json({ message: '데이터 형식이 올바르지 않습니다.' });
